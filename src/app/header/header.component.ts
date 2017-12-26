775e260272b0c94ead520f585b73a008401fcd58
Srivatsa S <srivatsashankaran@gmail.com>
2017-12-26 16:57:35 +0530

    Merged from master

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Component } from '@angular/core';
-=======
 import { Component, EventEmitter, Output } from '@angular/core';
->>>>>>> 392631671c372045fd555be54d919d72aff00049
 
 @Component({
     selector: 'app-header',
@@ -11,13 +7,9 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 
 export class HeaderComponent {
-<<<<<<< HEAD
-    
-=======
     @Output() featureSelected = new EventEmitter<string>();
 
     onSelect(feature: string) {
         this.featureSelected.emit(feature);
     }
->>>>>>> 392631671c372045fd555be54d919d72aff00049
 }
