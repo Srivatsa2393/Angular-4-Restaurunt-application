04d94ca8baa632783db8cdbd87008d1cb82ad21a
Srivatsa S <srivatsashankaran@gmail.com>
2017-12-29 20:19:53 +0530

    Passing dynamic parameters to the link

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -11,6 +11,8 @@ export class RecipeItemComponent implements OnInit {
 
   @Input() recipe: Recipe;
 
+  @Input() index: number;
+
   //@Output() recipeSelected = new EventEmitter<void>();
 
   //constructor(private recipeService: RecipeService) { }
